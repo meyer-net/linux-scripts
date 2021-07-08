2c519663ca31eec0d8c0c5790e969ce71319808f
meyer-net <meyer_net@foxmail.com>
2021-07-08 18:31:37 +0800

    fix var error

--- common/common_vars.sh
+++ common/common_vars.sh
@@ -20,7 +20,7 @@ DOWN_DIR=/tmp
 # 默认找最大的磁盘  ??? 优化为自动识别是否存在挂载第一个磁盘
 # MOUNT_ROOT=$(df -k | awk '{print $2}' | awk '{if (NR>2) {print}}' | awk 'BEGIN {max = 0} {if ($1+0 > max+0) {max=$1 ;content=$0} } END {print content}' | xargs -I {} sh -c 'df -k | grep "$1" | awk "{print \$NF}" | cut -c2' -- {})
 LSBLK_DISKS_STR=`lsblk | grep disk | awk 'NR==2{print $1}' | xargs -I {} echo '/dev/{}'`
-LSBLK_MOUNT_ROOT=`df -h | grep ${TMP_LSBLK_DISKS_STR:-":"} | awk -F' ' '{print $NF}'`
+LSBLK_MOUNT_ROOT=`df -h | grep ${LSBLK_DISKS_STR:-":"} | awk -F' ' '{print $NF}'`
 
 MOUNT_ROOT=${LSBLK_MOUNT_ROOT:-"/mountdisk"}
 MOUNT_DIR=${MOUNT_ROOT}/work
